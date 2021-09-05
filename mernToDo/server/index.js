30a5bb045e7eee68b99d6a4e4e29eb4bf30afd38
Fernando Torres <fernandot371@gmail.com>
2021-09-05 15:21:37 -0500

    client/server todo app MERN

---
+++ mernToDo/server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'; // before add this:"type": "module", on .json package => const express = require('express'); // if u dont not add module type
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import todosRouter from './routes/todos.js';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.use('/todos',todosRouter);
+
+const mongoDB = process.env.DB_MONGO;
+app.get('/', (req, res) => {
+  res.send('Welcome to server');
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(()=>{
+    console.log(`Server is running in port ${PORT}`);
+  })
+  .catch(err => console.log(err));
+
+app.listen(PORT);
